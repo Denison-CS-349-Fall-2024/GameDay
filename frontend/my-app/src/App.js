62cc8f4dbd835cef35681e54ef603e71d14e9d96
Thanh Do <69710821+do-t1@users.noreply.github.com>
2024-10-28 01:18:17 -0400

    change default page

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/home/home';
 import Login from './pages/Login/Login';
 import SignUpPage from './pages/signup/SignUp';
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
