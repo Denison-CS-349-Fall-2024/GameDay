bfa1b7f369f96c3b3891ab0d16b16770355b8806
Thanh Do <69710821+do-t1@users.noreply.github.com>
2024-10-27 20:31:18 -0400

    add calendar to home page

--- frontend/my-app/src/components/home/home.js
+++ frontend/my-app/src/components/home/home.js
@@ -1,6 +1,7 @@
 // src/components/home/home.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import ScheduleCalendar from '../schedule/scheduleCalendar';
 import './home.css';
 
 const HomeScreen = () => {
@@ -23,37 +24,34 @@ const HomeScreen = () => {
   };
 
   return (
-    <div className="home-container">
-      {/* Top header with Login and Sign Up buttons */}
-      <div className="top-header">
-        <h1>GameDay</h1>
-        <div className="top-buttons">
-          <button className="header-button" onClick={handleLogin}>Login</button>
-          <button className="header-button" onClick={handleSignUp}>Sign Up</button>
+    <div>
+      <div className="home-container">
+        {/* Top header with Login and Sign Up buttons */}
+        <div className="top-header">
+          <h1>GameDay</h1>
+          <div className="top-buttons">
+            <button className="header-button" onClick={handleLogin}>Login</button>
+            <button className="header-button" onClick={handleSignUp}>Sign Up</button>
+          </div>
+        </div>
+
+        {/* Main header hub */}
+        <div className="header">
+          <button className="header-button" onClick={handleNotificationsClick}>Notifications</button>
+          <button className="header-button" onClick={handleStandingsClick}>Standings</button>
         </div>
       </div>
-
-      {/* Main header hub */}
-      <div className="header">
-        <button className="header-button" onClick={handleNotificationsClick}>Notifications</button>
-        <button className="header-button" onClick={handleStandingsClick}>Standings</button>
+      
+      <div className='calendar-box'>
+        <ScheduleCalendar />
       </div>
-
-      {/* Calendar container */}
-      <div className="calendar-container">
-        <h2>Calendar</h2>
-        {/* Placeholder for Calendar */}
-        <div className="calendar-box">
-          <p>Calendar Display.</p>
-        </div>
-      </div>
-
-      {/* Footer with Contact Us and Meet the Team buttons */}
+      
       <div className="footer">
         <button className="footer-button">Contact Us</button>
         <button className="footer-button">Meet the Team</button>
       </div>
     </div>
+      
   );
 };
 
