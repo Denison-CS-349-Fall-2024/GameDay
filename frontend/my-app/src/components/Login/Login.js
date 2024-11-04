f35bf33589312f87f51dcd649cc0b7f60e9082b7
evlenny <116751501+evlenny@users.noreply.github.com>
2024-11-04 00:02:50 -0500

    added public ip4 address to login.js and signup.js

--- frontend/my-app/src/components/Login/Login.js
+++ frontend/my-app/src/components/Login/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
 
     // Send request to backend to check login info
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/login', {
+      const response = await fetch('http://54.198.164.179:5000/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
--- frontend/my-app/src/components/signup/SignUp.js
+++ frontend/my-app/src/components/signup/SignUp.js
@@ -23,7 +23,7 @@ const SignUpComponent = () => {
 
     // Send request to backend to check username info, if okay then store new information
     try {
-      const response = await fetch('http://127.0.0.1:5000/api/signup', {
+      const response = await fetch('http:/54.198.164.179:5000/api/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
