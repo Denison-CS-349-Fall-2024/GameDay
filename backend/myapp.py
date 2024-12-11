68465c9f9cef9471bbf7ce0652e596970098bafc
evlenny <116751501+evlenny@users.noreply.github.com>
2024-12-11 12:31:13 -0500

    open port to public

--- backend/myapp.py
+++ backend/myapp.py
@@ -230,5 +230,5 @@ def create_announcement():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=5000, debug=True)
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(port=5000, debug=True)
